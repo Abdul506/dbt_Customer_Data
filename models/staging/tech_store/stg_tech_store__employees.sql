7fa115bf4a8e81a1d2dfa8f7fb93d2c20d1bd540
Abdul506 <109149932+Abdul506@users.noreply.github.com>
2023-12-10 19:51:58 -0500

    Update stg_tech_store__employees.sql

--- models/staging/tech_store/stg_tech_store__employees.sql
+++ models/staging/tech_store/stg_tech_store__employees.sql
@@ -1,3 +1,10 @@
+{{
+  config(
+    materialized='incremental',
+    unique_key='employee_id',
+  )
+}}
+    
 with
 
 employees as (
@@ -13,7 +20,7 @@ final as (
         fname as first_name,
         lname as last_name,
         concat(first_name, ' ', last_name) as full_name,
-        hiredate as hired_at,
+        timestamp(left(`hiredate`, length(hiredate)-6)) as hired_at,
         nvl(enddate, '{{ var("default_date") }}') as terminated_at,
         iff(terminated_at is null, true, false) as is_active
 
